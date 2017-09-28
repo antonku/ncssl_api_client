6c5349a1d04c1e6bc6f7589250e8cfe3843abb34
Anton Kudinov <anton.ku@namecheap.com>
2017-09-28 20:00:15 +0300

    Update tests

--- tests/unit/test_flow_controller.py
+++ tests/unit/test_flow_controller.py
@@ -10,6 +10,16 @@ logging.disable(logging.CRITICAL)
 @mock.patch('ncssl_api_client.crypto.generator.CsrGenerator')
 class FlowControllerTest(TestCase):
 
+    supported_operations = (
+        'activate',
+        'reissue',
+        'create',
+        'retry_dcv',
+        'revoke',
+        'renew',
+        'create_and_activate',
+    )
+
     def test_throws_error_on_invalid_command(self, csr_generator_mock, api_client_mock, api_config_mock):
 
         controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
@@ -17,31 +27,24 @@ class FlowControllerTest(TestCase):
         with self.assertRaises(AttributeError):
             controller.execute('NON_EXISTENT_COMMAND')
 
-    def test_initiates_api_call_on_valid_command(self, csr_generator_mock, api_client_mock, api_config_mock):
+    def test_calls_csr_generator(self, csr_generator_mock, api_client_mock, api_config_mock):
 
-        controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
-
-        # TODO: replace this with some kind of data provider
-        for commandName in ('getinfo', 'activate', 'create', 'retry_dcv'):
-            controller.execute(commandName)
-            api_client_mock.send_call.assert_called()
-
-    def test_activate_command_calls_csr_generator(self, csr_generator_mock, api_client_mock, api_config_mock):
-
-        controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
-
-        controller.execute('activate')
-        csr_generator_mock.generate_csr.assert_called()
+        for command_name in ('activate', 'reissue'):
+            # TODO: subTest requires python3.4+, also available in 2.7 if unittest2 is in use
+            with self.subTest(command_name=command_name):
+                controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
+                controller.execute(command_name)
+                csr_generator_mock.generate_csr.assert_called()
 
     @mock.patch('ncssl_api_client.api.api_response.ApiResponse')
-    def test_execute_returns_api_response_instance_on_success(self, api_resp_mock, csr_generator_mock, api_client_mock, api_config_mock):
+    def test_execute_returns_api_response_instance(self, api_resp_mock, csr_generator_mock, api_client_mock, api_config_mock):
 
-        api_client_mock.send_call.return_value = api_resp_mock
-        controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
-
-        result = controller.execute('activate')
-        self.assertEqual(result, api_resp_mock)
-
+        for command_name in self.supported_operations:
+            with self.subTest(command_name=command_name):
+                api_client_mock.send_call.return_value = api_resp_mock
+                controller = FlowController(api_config_mock, {'common_name': 'example.com'}, api_client_mock, csr_generator_mock)
+                result = controller.execute(command_name)
+                self.assertEqual(result, api_resp_mock)
 
 
 
