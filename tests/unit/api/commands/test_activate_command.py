d75bc026686233a69b0346d0998502c5389720b7
Anton Kudinov <anton.ku@namecheap.com>
2017-11-04 18:56:23 +0200

    Update unit tests

--- tests/unit/api/commands/test_activate_command.py
+++ tests/unit/api/commands/test_activate_command.py
@@ -6,12 +6,11 @@ from ncssl_api_client.api.commands.activate_command import ActivateCommand
 @mock.patch('ncssl_api_client.api.api_client.ApiClient')
 class ActivateCommandTest(TestCase):
 
-
-    @mock.patch('ncssl_api_client.crypto.csr_generator.CsrGenerator')
-    def test_returns_api_response(self, csr_generator, api_client_mock):
-        pass
-        # command_params = {'common_name': 'test'}
-        # api_client_mock.send_call.return_value = 'test api response'
-        # command = ActivateCommand(command_params, api_client_mock)
-        # result = command.execute()
-        # self.assertEqual(result, 'test api response')
+    @mock.patch('ncssl_api_client.api.commands.activate_command.CsrGenerator')
+    @mock.patch('ncssl_api_client.api.commands.activate_command.ConfigManager')
+    def test_returns_api_response(self, csr_generator_mock, config_manager_mock, api_client_mock):
+        command_params = {'common_name': 'test'}
+        api_client_mock.send_call.return_value = 'test api response'
+        command = ActivateCommand(command_params, api_client_mock)
+        result = command.execute()
+        self.assertEqual(result, 'test api response')
--- tests/unit/api/commands/test_reissue_command.py
+++ tests/unit/api/commands/test_reissue_command.py
@@ -5,4 +5,12 @@ from ncssl_api_client.api.commands.reissue_command import ReissueCommand
 
 @mock.patch('ncssl_api_client.api.api_client.ApiClient')
 class ReissueCommandTest(TestCase):
-    pass
+
+    @mock.patch('ncssl_api_client.api.commands.activate_command.CsrGenerator')
+    @mock.patch('ncssl_api_client.api.commands.activate_command.ConfigManager')
+    def test_returns_api_response(self, csr_generator_mock, config_manager_mock, api_client_mock):
+        command_params = {'common_name': 'test'}
+        api_client_mock.send_call.return_value = 'test api response'
+        command = ReissueCommand(command_params, api_client_mock)
+        result = command.execute()
+        self.assertEqual(result, 'test api response')
