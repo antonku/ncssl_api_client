5724ef56e0934b8dd002b1da749baad1dcdcea46
Anton Kudinov <anton.ku@namecheap.com>
2017-12-04 17:45:13 +0200

    Fix tests

--- tests/unit/config/api/test_settings.py
+++ tests/unit/config/api/test_settings.py
@@ -1,5 +1,14 @@
 import mock
 from unittest import TestCase
+from ncssl_api_client.config.api import settings
+
+try:
+    reload  # Python 2.7
+except NameError:
+    try:
+        from importlib import reload  # Python 3.4+
+    except ImportError:
+        from imp import reload  # Python 3.0 - 3.3
 
 DATA = 'API_SETTINGS_TEST: TEST'
 
@@ -7,5 +16,5 @@ DATA = 'API_SETTINGS_TEST: TEST'
 class ApiSettingsLoadingTest(TestCase):
     @mock.patch("builtins.open", mock.mock_open(read_data=DATA))
     def test_update_locals(self):
-        import ncssl_api_client.config.api.settings
-        self.assertEqual(ncssl_api_client.config.api.settings.API_SETTINGS_TEST, 'TEST')
+        reload(settings)
+        self.assertEqual(settings.API_SETTINGS_TEST, 'TEST')
