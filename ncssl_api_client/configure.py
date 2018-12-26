1a7d922b5a8f22a7dbf033559673c1e3f286b254
Anton Ku <anton.ku.nc@gmail.com>
2018-12-26 08:44:41 +0200

    NCSSLAPI-7 Use yaml.safe_dump for initial config generation

--- ncssl_api_client/configure.py
+++ ncssl_api_client/configure.py
@@ -66,7 +66,7 @@ def setup_configs():
             source_api_config = yaml.load(f)
             api_config = update_with_user_api_info(source_api_config)
             stream = open(user_api_config_path, 'w')
-            yaml.dump(api_config, stream, default_flow_style=False, indent=4, width=79)
+            yaml.safe_dump(api_config, stream, default_flow_style=False, indent=4, width=79)
 
     user_crypto_config_path = DELIMITER.join([CRYPTO_CONFIG_PATH, SETTINGS_FILE_NAME])
     if not os.path.exists(user_crypto_config_path):
